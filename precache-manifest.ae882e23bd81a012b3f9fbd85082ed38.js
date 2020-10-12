29409a89ca962d7cd0d1cf3fa93173bd9137a3f0
Elina <elisonfog@gmail.com>
2020-10-12 20:16:05 +0300

    Updates

---
+++ precache-manifest.ae882e23bd81a012b3f9fbd85082ed38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed6852d2586aae340b44f1c20dadeabd",
+    "url": "/02-phonebook/index.html"
+  },
+  {
+    "revision": "61556eef12253b94290a",
+    "url": "/02-phonebook/static/js/2.b71ed7c5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/02-phonebook/static/js/2.b71ed7c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "684952f0f4ec34c61724",
+    "url": "/02-phonebook/static/js/main.75849d9d.chunk.js"
+  },
+  {
+    "revision": "feedaa000d9d65a856bf",
+    "url": "/02-phonebook/static/js/runtime-main.ae15dc1f.js"
+  }
+]);
